b15332960b0748cc21d21fd1a3f4e28b5bde6191
Mat Lundin <matmll12@gmail.com>
2021-11-29 17:29:42 -0600

    attempted to set state based on nav bar buttons

--- my-portfolio/src/components/nav.js
+++ my-portfolio/src/components/nav.js
@@ -1,19 +1,28 @@
-import React from 'react';
-
+// import React, {useState} from 'react';
+import React from 'react'
 // use a state var to keep track of active tab?
-// const [activeTab, setActiveTab] = useState('home');
+
 
 const navbar = () => {
-
+// const [activeTab, setActiveTab] = useState('home');
     return (
         
     <nav>
     <div class="nav-wrapper">
       <a href="#" class="left">Mat Lundin</a>
       <ul id="nav-mobile" class="right ">
-        <li class="active"><a href="">About</a></li>
-        <li><a href="">Projects</a></li>
-        <li><a href="">Contact</a></li>
+        <li class="active"
+        // onClick={()=> MediaStreamAudioDestinationNode('about')}
+        >
+            <a href="">About</a></li>
+        <li
+        // onClick={()=> MediaStreamAudioDestinationNode('projects')}
+        >
+            <a href="">Projects</a></li>
+        <li
+        // onClick={()=> MediaStreamAudioDestinationNode('contact')}
+        >
+            <a href="">Contact</a></li>
       </ul>
     </div>
   </nav>
