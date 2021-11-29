75c4cd2060556799660d11544fdf4157ef6a9c90
Mat Lundin <matmll12@gmail.com>
2021-11-29 16:52:53 -0600

    added materialize cdn and got the navbar to stick to the top

---
+++ my-portfolio/src/components/header.js
@@ -0,0 +1,14 @@
+import Nav from './nav'
+
+const header = () => {
+    return(
+        <div class="container">
+        <header>
+            <Nav />
+        </header>
+        </div>
+        
+    )
+}
+
+export default header
---
+++ my-portfolio/src/components/nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const navbar = () => {
+
+    return (
+        
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="left">Mat Lundin</a>
+      <ul id="nav-mobile" class="right ">
+        <li class="active"><a href="">Home</a></li>
+        <li><a href="">Projects</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+  
+    )
+
+}
+
+export default navbar
