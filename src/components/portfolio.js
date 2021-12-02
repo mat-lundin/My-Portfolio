0ef517a30796aae77db38b255b5a245d3cee537e
Mat Lundin <matmll12@gmail.com>
2021-12-01 18:30:34 -0600

    fixed portfolio map function

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -2,29 +2,27 @@ import React from 'react';
 import portfolioData from '../portfolio.json';
 
 const portfolio = function(){
+    return (
     portfolioData.map((project) => {
     return (
-        <>
-        <div className="row">
-    <div className="col s12 m7">
+        // <div className="row">
+    <div key= {project.name} className="col s4 m7">
       <div className="card">
         <div className="card-image">
           <img src="images/sample-1.jpg"></img>
           <span className="card-title">{project.name}</span>
         </div>
         <div className="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
+          <p>{project.description}</p>
         </div>
         <div className="card-action">
-          <a href="#">This is a link</a>
+          <a href={project.url}>Visit</a>
         </div>
       </div>
     </div>
-  </div>
-  </>
+//   </div>
     )
-    })
+    }));
 };
 
 export default portfolio;
