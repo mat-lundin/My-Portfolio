5e7e3f9585a20cfe2d1a4a3c26cd626b5f1ee3b4
Mat Lundin <matmll12@gmail.com>
2021-12-02 17:27:40 -0600

    css fun, cleanup

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const footer = () => {
           <div className="row">
             <div className="col l6 s12">
               <h5 className="white-text">Visit me!</h5>
-              <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              {/* <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p> */}
             </div>
             <div className="col l4 offset-l2 s12">
               <h5 className="white-text">Links</h5>
@@ -21,7 +21,7 @@ const footer = () => {
         <div className="footer-copyright">
           <div className="container">
           Built in React by Mat Lundin
-          <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+          {/* <a className="grey-text text-lighten-4 right" href="#!">More Links</a> */}
           </div>
         </div>
       </footer>
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -6,17 +6,18 @@ const portfolio = function () {
     <div className="row">
    {portfolioData.map((project) => {
     return (
-        <div key={project.name} className="col s7 m4 l3">
-          <div className="card" style={{ width: '18rem' }}>
+        <div key={project.name} className="col s7 m6 l3">
+          <div className="card" style={{ width: '20rem' }}>
             <div className="card-image">
               <img className="responsive-img" src={process.env.PUBLIC_URL + project.image}></img>
-              <span className="card-title">{project.name}</span>
+              <span className="card-title" style={{backgroundColor: "black", opacity: 0.5}}>{project.name}</span>
             </div>
             <div className="card-content">
               <p>{project.description}</p>
             </div>
             <div className="card-action">
-              <a href={project.url}>Visit</a>
+              {project.url && <a href={project.url}>Visit</a>}
+              <a href={project.repoUrl}>Github</a>
             </div>
           </div>
         </div>
