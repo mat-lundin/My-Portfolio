4b1bd2e79b7846fce3c058b3361ac5eb74758169
Mat Lundin <matmll12@gmail.com>
2021-12-04 20:55:28 -0600

    added more icons for links

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,8 +10,10 @@ const footer = () => {
               {/* <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p> */}
               <h5 className="white-text">Visit me!</h5>
               <ul>
-                <li><a className="grey-text text-lighten-3" href="https://github.com/mat-lundin" target="_blank">GitHub</a></li>
-                <li><a className="grey-text text-lighten-3" href="https://www.linkedin.com/in/mathew-lundin/" target="_blank">LinkedIn</a></li>
+                {/* <li><a className="grey-text text-lighten-3" href="https://github.com/mat-lundin" target="_blank">GitHub</a></li>
+                <li><a className="grey-text text-lighten-3" href="https://www.linkedin.com/in/mathew-lundin/" target="_blank">LinkedIn</a></li> */}
+                <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/mat-lundin" role="button" ><i class="fab fa-github" ></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/mathew-lundin/" role="button"><i class="fab fa-linkedin-in"></i></a>
               </ul>
             </div>
             <div className="col l4 offset-l2 s12">
