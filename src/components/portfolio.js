4eeab497bac735393e63ffbc3bedacc171e9652a
Mat Lundin <matmll12@gmail.com>
2021-12-20 17:58:42 -0600

    replaced pink overlay project titles, gave the portfolio cards borders

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -6,12 +6,13 @@ const portfolio = function () {
     <div className="container">
     <div className="row">
     <div key={portfolioData[0].name} className="col s7 m6 l12">
-          <div className="card large hoverable" style={{ width: '20rem' }}>
+          <div className="card large hoverable portfolio-card" style={{ width: '20rem' }}>
             <div className="card-image">
               <img className="responsive-img" src={process.env.PUBLIC_URL + portfolioData[0].image} alt={portfolioData[0].name}></img>
-              <span className="card-title" style={{backgroundColor: "pink", opacity: 0.89, color: 'black', padding: 0}}>{portfolioData[0].name}</span>
+              {/* <span className="card-title" style={{backgroundColor: "pink", opacity: 0.89, color: 'black', padding: 0}}>{portfolioData[0].name}</span> */}
             </div>
             <div className="card-content">
+            <b>{portfolioData[0].name}</b>
               <p>{portfolioData[0].description}</p>
             </div>
             <div className="card-action">
@@ -29,12 +30,13 @@ const portfolio = function () {
      if (project !== portfolioData[0])
     return (
         <div key={project.name} className="col s7 m4 l4">
-          <div className="card medium hoverable" style={{ width: '20rem' }}>
+          <div className="card hoverable portfolio-card" style={{ width: '20rem' }}>
             <div className="card-image">
               <img className="responsive-img" src={process.env.PUBLIC_URL + project.image} alt={project.name}></img>
-              <span className="card-title" style={{backgroundColor: "pink", opacity: 0.89, color: 'black', padding: 0}}>{project.name}</span>
+              {/* <span className="card-title" style={{backgroundColor: "pink", opacity: 0.89, color: 'black', padding: 0}}>{project.name}</span> */}
             </div>
             <div className="card-content">
+              <b>{project.name}</b>
               <p>{project.description}</p>
             </div>
             <div className="card-action">
