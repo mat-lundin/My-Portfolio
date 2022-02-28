f20a29a3f94616f3e9e97687f5c1e1e81f4cf0d3
Mat Lundin <matlundin.dev@gmail.com>
2022-02-27 23:38:55 -0600

    adjusted email placement, updated Sheltr description

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,13 +9,13 @@ const footer = () => {
               {/* <h5 className="white-text">Visit me!</h5> */}
               {/* <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p> */}
               <h5 className="white-text">Contact me!</h5>
+                <a className="white-text" href="mailto: matlundin.dev@gmail.com">matlundin.dev@gmail.com</a>
               <ul>
                 {/* <li><a className="grey-text text-lighten-3" href="https://github.com/mat-lundin" target="_blank">GitHub</a></li>
                 <li><a className="grey-text text-lighten-3" href="https://www.linkedin.com/in/mathew-lundin/" target="_blank">LinkedIn</a></li> */}
                 <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/mat-lundin" role="button"><i className="fab fa-github" ></i></a>
                 <a className="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/mathew-lundin/" role="button"><i className="fab fa-linkedin-in"></i></a>
-                <br/>
-                <a className="white-text" href="mailto: matlundin.dev@gmail.com">matlundin.dev@gmail.com</a>
+                
               </ul>
             </div>
             <div className="col l4 offset-l2 s12">
